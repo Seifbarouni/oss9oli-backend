08fd494c4772cb287f03710aa176ff03f2a33021
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2022-08-09 23:15:54 +0100

    auth

--- users-microservice/controllers/userController.js
+++ users-microservice/controllers/userController.js
@@ -1,97 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const OAuth2Client = require("google-auth-library").OAuth2Client;
 
 const User = require("../models/userModel");
 
-const googleClient = new OAuth2Client({
-  clientId: process.env.GOOGLE_CLIENT_ID,
-});
-
-// @desc    Authenticate user using google oauth
-// @route   POST /api/v1/auth/google
-// @access  Public
-
-const authenticateUserGoogle = asyncHandler(async (req, res) => {
-  const { token } = req.body;
-  if (!token) {
-    return res.status(400).json({
-      success: false,
-      error: "Token is required",
-    });
-  }
-  const ticket = await googleClient.verifyIdToken({
-    idToken: token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
-
-  const payload = ticket.getPayload();
-  const user = await User.findOne({
-    email: payload.email,
-    name: payload.name,
-    avatar: payload.picture,
-  });
-
-  if (!user) {
-    try {
-      const newUser = await User.create({
-        providerId: payload.sub,
-        name: payload.name,
-        avatar: payload.picture,
-        email: payload.email,
-        pack: "free",
-        role: "user",
-      });
-      return res.status(201).json({
-        success: true,
-        data: newUser,
-      });
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).send("Server Error");
-    }
-  } else {
-    return res.status(200).json({
-      success: true,
-      data: user,
-    });
-  }
-});
-
-// @desc    Authenticate user using facebook oauth
-// @route   POST /api/v1/auth/facebook
-// @access  Public
-
-const authenticateUserFacebook = asyncHandler(async (req, res) => {
-  const { token } = req.body;
-  const user = await User.findOne({
-    providerId: token,
-  });
-
-  if (!user) {
-    try {
-      const newUser = await User.create({
-        providerId: token,
-        name: req.body.name,
-        avatar: req.body.picture,
-        email: req.body.email,
-        pack: "free",
-        role: "user",
-      });
-      return res.status(201).json({
-        success: true,
-        data: newUser,
-      });
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).send("Server Error");
-    }
-  } else {
-    return res.status(200).json({
-      success: true,
-      data: user,
-    });
-  }
-});
 
 // @desc    Update user
 // @route   POST /api/v1/
@@ -114,8 +24,29 @@ const updateUser = asyncHandler(async (req, res) => {
   });
 });
 
+// @desc    create user if !exist
+// @access  private
+
+const createUser = async (providerId, name, avatar, email)=>{
+  //search if user exists
+  const user = await User.findOne({
+    providerId: providerId,
+  });
+  //if user don't exist -> create one
+  if (!user) {
+      user  = await User.create({
+        providerId: providerId,
+        name: name,
+        avatar: avatar,
+        email: email,
+        pack: "free",
+        role: "user",
+      });
+    }
+    return user
+}
+
 module.exports = {
-  authenticateUserGoogle,
-  authenticateUserFacebook,
   updateUser,
+  createUser
 };
--- users-microservice/routes/userRoutes.js
+++ users-microservice/routes/userRoutes.js
@@ -2,13 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  authenticateUserGoogle,
-  authenticateUserFacebook,
   updateUser,
 } = require("../controllers/userController");
 
-router.route("/auth/google").post(authenticateUserGoogle);
-router.route("/auth/facebook").post(authenticateUserFacebook);
+
 router.route("/:id").post(updateUser);
 
 module.exports = router;
