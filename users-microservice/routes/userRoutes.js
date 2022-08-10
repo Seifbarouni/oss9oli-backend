e1b25b001db80c5193fcf9d1e531da5bd9f039c2
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2022-08-10 10:48:20 +0100

    add auth middleware to user service

--- users-microservice/routes/userRoutes.js
+++ users-microservice/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken, verifyProperty } = require("../middlewares/auth");
 
 const {
   updateUser,
 } = require("../controllers/userController");
 
 
-router.route("/:id").post(updateUser);
+router.post("/:id",[verifyToken, verifyProperty],updateUser);
 
 module.exports = router;
