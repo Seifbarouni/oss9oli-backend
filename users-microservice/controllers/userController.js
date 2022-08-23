0db6a11f94727fcf548323322a6d02490ed03c68
Seifbarouni <seif.barouni11@gmail.com>
2022-08-23 04:45:53 +0100

    upload new user image

--- users-microservice/controllers/userController.js
+++ users-microservice/controllers/userController.js
@@ -1,28 +1,68 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
 
 const axios = require("axios")
 
 const User = require("../models/userModel");
 
+// @desc    Get user uploaded image
+// @route   GET /api/v1/image
+// @acces   Public
+const getUserUploadedImage = asyncHandler(async(req,res)=>{
+  try {
+   const user = await User.findById(req.body.payload.userId) 
+   return res.status(200).json({
+      image: user.image
+    })
+  } catch (error) {
+   return res.status(500).json({
+      error: error
+    })
+  }
+}) 
 
 // @desc    Update user
 // @route   POST /api/v1/
 // @access  Public
 
 const updateUser = asyncHandler(async (req, res) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let user;
+  if (req.file){
+    const image = {
+      data : req.file.buffer.toString("base64"),
+      contentType : req.file.mimetype
+    }
+    user = await User.findOneAndUpdate(
+      {_id: req.body.payload.userId},
+      {image, ...req.body},
+      {new: true, runValidators:true}
+    )
+  }else{
+     user = await User.findOneAndUpdate(
+      {_id: req.body.payload.userId},
+      req.body,
+      {new: true, runValidators:true}
+    )
+
+  }
   if (!user) {
     return res.status(404).json({
       success: false,
       message: "User not found",
     });
   }
+  const isImagePresent = false
+  if (user.image) isImagePresent = true
+  const JWToken = jwt.sign(
+  { userId: user._id, name: user.name, picture: user.avatar, pack: user.pack, isImagePresent},
+          process.env.JWT_SECRET
+  );
+
+ 
   return res.status(200).json({
     success: true,
-    data: user,
+    data: JWToken,
   });
 });
 
@@ -64,5 +104,6 @@ const createUser = async (providerId, name, avatar, email)=>{
 }
 module.exports = {
   updateUser,
-  createUser
+  createUser,
+  getUserUploadedImage
 };
--- users-microservice/routes/userRoutes.js
+++ users-microservice/routes/userRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
 const { verifyToken, verifyProperty } = require("../middlewares/auth");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single("file");
+
 
 const {
   updateUser,
+  getUserUploadedImage
 } = require("../controllers/userController");
 
 
-router.post("/:id",[verifyToken, verifyProperty],updateUser);
+router.get("/image",[verifyToken, verifyProperty],getUserUploadedImage);
+router.post("/",[upload,verifyToken, verifyProperty],updateUser);
 
 module.exports = router;
